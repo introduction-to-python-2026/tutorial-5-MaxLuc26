2dbb21598e3bd2ebc2eb92ae5c2c8138e8c24b1c
MaxLuc26 <maxlucasberman@gmail.com>
2025-11-26 16:31:58 +0200

    Update histogram_utils.py

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,7 +1,13 @@
-# Q: Write an import statement that imports the function pyplot from the module matplotlib and renames it to plt.
+import histogram.ipynb
 
 def build_histogram(data):
-    pass # Replace the `pass` with your code
+    histogram_dict = {}
+    for i in  data: 
+      if i in histogram_dict:
+        histogram_dict[i] += 1
+      else:
+        histogram_dict[i] = 1
+    return histogram_dict
 
 def plot_histogram(histogram):
     x_values = list(histogram.keys())
